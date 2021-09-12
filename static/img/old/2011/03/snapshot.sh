42a0b6866f2a66ccc65816fe1907b76b64838d3b
Scott O'Brien <sob@fb.com>
2021-09-11 23:35:08 -0700

    Added new blog post, with new hugo

--- static/img/old/2011/03/snapshot.sh
+++ static/img/old/2011/03/snapshot.sh
@@ -1,3 +1,25 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b1e7c13c1da6c0ad16821e32062c89d04ef67195a5ae41fb72de1a85cef0150c
-size 610
+#!/bin/bash
+
+#print out usage
+if [ $# -ne 2 ]
+then
+        echo "Usage: snapshot.sh [snapName] [max]"
+        echo "  eg. snapshot.sh hour 24"
+fi
+
+#if the max snapshot already exists, just delete it
+if [ `zfs list -t snapshot | grep datastore@$1.$2 | wc -l` -eq 1 ]
+then
+        zfs destroy -r datastore@$1.$2
+fi
+
+#
+for ((i=$2-1; i >= 0; i--)); do
+        if [ `zfs list -t snapshot | grep datastore@$1.$i | wc -l` -eq 1 ]
+        then
+                #this snapshot exists, so we want to move it up one
+                zfs rename -r datastore@$1.$i @$1.$[$i+1]
+        fi
+done
+
+zfs snapshot -r datastore@$1.0
