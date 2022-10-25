491c599d812532fe317036e339bcd214111c90b4
Scott O'Brien <scott@scottyob.com>
2022-10-24 22:26:26 -0700

    Flying site with stats is usable, but not yet accurate as it needs updating

--- import-tracklogs.sh
+++ import-tracklogs.sh
@@ -2,7 +2,6 @@
 
 # cat data/flightlog.json | jq -r 'map(.location) | unique | del(.. | nulls)[]'
 cat data/flightlog.json | jq 'map(. + {type: "flightlog", "flying-location": .location})[]' -c | while read line; do
-    echo "Processing line"
     flightNum=$(echo "${line}" | jq '.flightNumber')
     echo "${line}" > content/flying/flights/${flightNum}.md
 done
